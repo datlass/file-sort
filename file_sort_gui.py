e4ac5d03070343916cfc3cf966328d31c54d88f0
datlass <dismadi01@gmail.com>
2020-06-29 17:27:39 +0800

    Transforms file-sort into a function

--- file_sort_gui.py
+++ file_sort_gui.py
@@ -4,7 +4,7 @@ root = Tk()  # create window
 
 # Label widget
 myLabel = Label(root, text="Hello World")
-myButton = Button(root, text="Click me").pack()
+myButton = Button(root, text="Click me").grid(row=1,column=1)
 
 myLabel.grid(row=0,column=0)
 
