448f6cc039ebd6a50e3d71076a1468b502e622e5
datlass <dismadi01@gmail.com>
2020-06-30 16:38:27 +0800

    reformatted to proper naming standard

--- practice_gui.py
+++ practice_gui.py
@@ -3,15 +3,19 @@ from tkinter import *
 root = Tk()  # create window called root
 
 e = Entry(root, width=50)
-e.pack()#Better to seperate in order to define e variable
-e.insert(0,"Enter name here")
+
+e.pack()  # Better to seperate in order to define e variable
+
+e.insert(0, "Enter name here")
+
+
 # Label widget
-def myClick():
-    myLabel = Label(root, text = e.get() )
-    myLabel.pack()
+def myclick():
+    my_label = Label(root, text=e.get())
+    my_label.pack()
     print(e.get())
 
-myButton = Button(root, text="Enter name", command = myClick, padx=50).pack()
 
+myButton = Button(root, text="Enter name", command=myclick(), padx=50).pack()
 
 root.mainloop()
