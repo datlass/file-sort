e2f12b4ebeb2f2b4544713e6829d14d139a8443b
datlass <dismadi01@gmail.com>
2020-06-30 16:59:59 +0800

    Added a title and organized it in a grid

--- file_sort_gui.py
+++ file_sort_gui.py
@@ -1,14 +1,22 @@
 from tkinter import *
 from tkinter import filedialog
-
+from file_sort import sort_folder
 root = Tk()  # create window called root
+root.title("Dimas's Organizing Program")
 
+
+e = Entry()
+e.grid(row = 0,column = 0)
+
+#Function to select the directory
 def askdir():
     folder = filedialog.askdirectory()
-    print("This folder has been selected: ")
+    print("This folder has been selected to sort: ")
     print(folder)
+    e.insert(0,folder)
 
-my_button = Button(root, text="Search organizing folder", command= askdir, padx=50)
-my_button.pack()
+
+my_button = Button(root, text="Search folder to organize", command= askdir)
+my_button.grid(row = 0,column = 1)
 
 root.mainloop()
