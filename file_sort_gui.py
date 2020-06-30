13d61fd639e9a618e8c059a9d576b918141ab548
datlass <dismadi01@gmail.com>
2020-06-30 16:50:06 +0800

    Added directory selection button to program

--- file_sort_gui.py
+++ file_sort_gui.py
@@ -1,5 +1,14 @@
 from tkinter import *
+from tkinter import filedialog
 
 root = Tk()  # create window called root
+
+def askdir():
+    folder = filedialog.askdirectory()
+    print("This folder has been selected: ")
+    print(folder)
+
+my_button = Button(root, text="Search organizing folder", command= askdir, padx=50)
+my_button.pack()
 
 root.mainloop()
