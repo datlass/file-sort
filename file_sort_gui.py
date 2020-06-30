12f65c481121ada198d581d140ad1fd0d98c23f3
datlass <dismadi01@gmail.com>
2020-06-30 16:19:08 +0800

    tkinter entry fixed

--- file_sort_gui.py
+++ file_sort_gui.py
@@ -1,14 +1,19 @@
 from tkinter import *
 
-root = Tk()  # create window
+root = Tk()  # create window called root
+
+
+e = Entry(root, width=50)
+e.pack()#Better to seperate in order to define e variable
 
 # Label widget
 def myClick():
-    myLabel = Label(root, text="Button Clicked")
+    myLabel = Label(root, text = e.get() )
     myLabel.pack()
+    print(e.get())
 
 
-myButton = Button(root, text="Click me", command = myClick, padx=50).pack()
+myButton = Button(root, text="Enter name", command = myClick, padx=50).pack()
 
 
 root.mainloop()
