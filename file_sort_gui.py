3f1ba0986f89f67ad487bc2b749ef92e05175b2a
datlass <dismadi01@gmail.com>
2020-06-30 16:32:42 +0800

    Seperates practice code into seperate file

--- file_sort_gui.py
+++ file_sort_gui.py
@@ -2,18 +2,4 @@ from tkinter import *
 
 root = Tk()  # create window called root
 
-
-e = Entry(root, width=50)
-e.pack()#Better to seperate in order to define e variable
-
-# Label widget
-def myClick():
-    myLabel = Label(root, text = e.get() )
-    myLabel.pack()
-    print(e.get())
-
-
-myButton = Button(root, text="Enter name", command = myClick, padx=50).pack()
-
-
 root.mainloop()
