d3257b18fc8af5bf9f8abb8621b0c8f503313f7d
datlass <dismadi01@gmail.com>
2020-06-30 17:22:25 +0800

    Added a message popup box to ask confirmation

--- file_sort_gui.py
+++ file_sort_gui.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from file_sort import sort_folder
 root = Tk()  # create window called root
 root.title("Dimas's Organizing Program")
@@ -15,8 +15,19 @@ def askdir():
     print(folder)
     e.insert(0,folder)
 
+def organize():
+    print("test")
+    response = messagebox.askokcancel("Organize Folder","Are you sure you want to organize the folder?")
+    print(response)
+    if response is True:
+        print("is true lol")
+        #sort_folder(e.get()) #Needs an error message in case mypath is wrong
+
 
 my_button = Button(root, text="Search folder to organize", command= askdir)
 my_button.grid(row = 0,column = 1)
+
+execute_ocd= Button(root, text="Organize this folder", command= organize)
+execute_ocd.grid(row = 1,column = 0)
 
 root.mainloop()
