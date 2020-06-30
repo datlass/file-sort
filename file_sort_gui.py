d2252946bf26cdf937c1f63f96453e902c5d4b8b
datlass <dismadi01@gmail.com>
2020-06-30 16:11:51 +0800

    Experimented with button clicking to print text

--- file_sort_gui.py
+++ file_sort_gui.py
@@ -3,9 +3,12 @@ from tkinter import *
 root = Tk()  # create window
 
 # Label widget
-myLabel = Label(root, text="Hello World")
-myButton = Button(root, text="Click me").grid(row=1,column=1)
+def myClick():
+    myLabel = Label(root, text="Button Clicked")
+    myLabel.pack()
 
-myLabel.grid(row=0,column=0)
+
+myButton = Button(root, text="Click me", command = myClick, padx=50).pack()
+
 
 root.mainloop()
