207f9376fc524fe6efb26145a54633ab2ff2a6aa
datlass <dismadi01@gmail.com>
2020-07-01 12:35:31 +0800

    Changed the positioning+typo check

--- file_sort_gui.py
+++ file_sort_gui.py
@@ -3,15 +3,16 @@ from tkinter import filedialog, messagebox
 from file_sort import sort_folder
 root = Tk()  # create window called root
 root.title("Dimas's Organizing Program")
+root.configure(background= '#ededed')
 
-# Gets the requested values of the height and widht.
+# Gets the requested values of the height and width.
 windowWidth = root.winfo_reqwidth()
 windowHeight = root.winfo_reqheight()
 print("Width",windowWidth,"Height",windowHeight)
 
 # Gets both half the screen width/height and window width/height
-positionRight = int(root.winfo_screenwidth()/2 - windowWidth/2)
-positionDown = int(root.winfo_screenheight()/3 - windowHeight/2)
+positionRight = int(root.winfo_screenwidth()/3 - windowWidth/2)
+positionDown = int(root.winfo_screenheight()/2 - windowHeight/2)
 
 # Positions the window in the center of the page.
 root.geometry("+{}+{}".format(positionRight, positionDown))
