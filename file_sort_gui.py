cb2d30be40c50a9d3ecc0b6da185c0b1c998c250
datlass <dismadi01@gmail.com>
2020-07-06 17:31:46 +0800

    Added a comment and changed width of listbox

--- file_sort_gui.py
+++ file_sort_gui.py
@@ -4,7 +4,9 @@ from file_sort import sort_folder, filesearch
 
 root = Tk()  # create window called root
 root.title("Dimas's Organizing Program")
-root.configure(background='#ededed')
+#Adds color to the root
+mac_grey = '#ededed'
+root.configure(background=mac_grey)
 
 # Gets the requested values of the height and width.
 windowWidth = root.winfo_reqwidth()
@@ -22,13 +24,13 @@ root.geometry("+{}+{}".format(positionRight, positionDown))
 widthx = 25
 
 # Adds title to the python window
-my_title = Label(root, text="Insert directory to organize")
+my_title = Label(root, text="Insert directory to organize",borderwidth=2, relief="groove")
 
 e = Entry()
 e.insert(0, "Input directory here")
 
 # Function to select the directory for the search button
-frame = Frame(root,bg='gray')
+frame = Frame(root)
 list_label = Label(frame, text="Files within selected directory:")
 file_list = Listbox(frame)
 
@@ -66,9 +68,13 @@ execute_ocd = Button(root, text="Organize this folder", command=organize)
 #Adds a scrollbar to the frame and tell it to follow the listbox
 file_scroll_y = Scrollbar(frame)
 file_scroll_x = Scrollbar(frame, orient = HORIZONTAL)
-#scrollbar.config(command = file_list.yview)
 #scrollbar.grid( row=4, column=2 , columnspan=3,)
+
+#Attaches scrollbar to file list
+file_scroll_y.config(command = file_list.yview)
 file_list.config(yscrollcommand = file_scroll_y.set)
+
+file_scroll_x.config(command = file_list.xview)
 file_list.config(xscrollcommand = file_scroll_x.set)
 
 #Organizes the listbox components within the frame using grid system
@@ -78,11 +84,11 @@ file_scroll_x.grid(row=1, column=0,sticky='SEW')
 file_list.grid(row=1, column=0)
 
 # All the Elements
-my_title.grid(row=0, column=0, columnspan=3)
+my_title.grid(row=0, column=0, columnspan=3,pady=(10,5))
 e.grid(row=1, column=0)
 my_button.grid(row=1, column=1)
 execute_ocd.grid(row=1, column=2)
-file_list.config(width=50)
+file_list.config(width=35)
 #list_label.grid(row=3, column=0,pady=5)
 frame.grid(row=4, column=0, columnspan=3,pady=25)
 #file_list.grid(row=4, column=0, columnspan=3)
